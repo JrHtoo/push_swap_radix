9543876b5e00bb69ec1a21d6cc3cb8c49f7ee416
Junior Htoo Htoo <juhtoo-h@z2t17c4.42bangkok.com>
2025-02-10 12:29:01 +0700

    norminette checked

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: juhtoo-h <juhtoo-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 15:50:53 by juhtoo-h          #+#    #+#             */
-/*   Updated: 2025/02/05 12:39:39 by juhtoo-h         ###   ########.fr       */
+/*   Updated: 2025/02/10 12:28:29 by juhtoo-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,4 +99,3 @@ void	index_init(t_list **stack)
 		head = get_next_min(stack);
 	}
 }
-
